ed02f03a9d45b67f7010f042c500b94b4931186d
Chris Staton <topherstaton@gmail.com>
2025-02-02 14:05:46 -0800

    Update with recent projects

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.scss";
 import Project from "./Project";
-import LSSImg from "./images/LSS.jpg";
-import SMImg from "./images/SM.jpg";
+import TMGImg from "./images/TMG.jpg";
+import EMCImg from "./images/EMC.jpg";
 import CCRImg from "./images/CCR.jpg";
 import DevTimeline from "./DevTimeline";
 import ContactForm from "./ContactForm";
@@ -14,124 +14,122 @@ import { useEffect, useState } from "react";
 const TRACKING_ID = "UA-245661443-2"; // OUR_TRACKING_ID
 ReactGA.initialize(TRACKING_ID);
 
+const CURRENT_YEAR = new Date().getFullYear();
+
 function App() {
-	const [isDark, setIsDark] = useState(false);
-	const [click, setClick] = useState(0);
+    const [isDark, setIsDark] = useState(false);
+    const [click, setClick] = useState(0);
 
-	const switchTheme = () => {
-		setIsDark(!isDark);
-	};
 
-	useEffect(() => {
-		setClick(1);
-		setInterval(() => {
-			setClick(1);
-		}, [8000]);
-	}, []);
+    const switchTheme = () => {
+        setIsDark(!isDark);
+    };
 
-	const gaEventTracker = useAnalyticsEventTracker("Link Click");
+    useEffect(() => {
+        setClick(1);
+        setInterval(() => {
+            setClick(1);
+        }, [8000]);
+    }, []);
 
-	useEffect(() => {
-		ReactGA.pageview(window.location.pathname + window.location.search);
-	}, []);
+    const gaEventTracker = useAnalyticsEventTracker("Link Click");
 
-	return (
-		<div className={`App ${isDark ? "dark-mode" : null}`}>
-			<button
-				className="darkmode"
-				onClick={() => {
-					switchTheme();
-					gaEventTracker("Dark Mode");
-				}}
-			>
-				{isDark ? (
-					<img className="icon" src={Sun} alt="light-mode" />
-				) : (
-					<img className="icon" src={Moon} alt="dark-mode" />
-				)}
-			</button>
-			<div className="wrapper">
-				<section className="intro">
-					<h1
-						id='my-name'
-						onClick={() => setClick(1)}
-						className="intro__h1 shine"
-						onAnimationEnd={() => setClick(0)}
-						click={click}
-					>Chris Staton</h1>
-					<p className="intro__title">Software Engineer & Web Developer</p>
-					<p className="intro__paragraph">
-						As a software engineer, I continuously seek emerging
-						technologies to master. Every project I undertake is
-						meticulously crafted from inception to execution,
-						reflecting a blend of thoughtful planning, design,
-						and implementation. I'm deeply passionate about
-						creating not just applications, but distinct digital
-						brands accentuated with robust SEO and user-centric UI.
-						With a foundation in graphic design and marketing, I
-						prioritize user experience in every application I develop,
-						ensuring intuitive and engaging interactions.
-					</p>
-				</section>
-				<section className="projects">
-					<Project
-						title="Project 3"
-						techTags={["React Native?", "TBD1", "TBD2"]}
-						link="https://www.livestreamsearch.com"
-						domain="project3.com"
-						github="https://github.com/c-staton/livestream-search"
-					/>
-					<Project
-						title="ScapeMaps"
-						techTags={["React", "Python", "Flask"]}
-						image={SMImg}
-						link="https://scapemaps.com"
-						domain="scapemaps.com"
-						github="https://github.com/c-staton/billdivider"
-					/>
-					<Project
-						title="Casa Carlos"
-						techTags={["React", "JavaScript", "CSS"]}
-						image={CCRImg}
-						link="https://casacarlosrestaurant.com"
-						domain="casacarlosrestaurant.com"
-						github="https://github.com/c-staton/sharefolio"
-					/>
-				</section>
-				<section className="timeline">
-					<DevTimeline />
-				</section>
-				<section className="contact">
-					<ContactForm />
-				</section>
-				<section className="footer">
-					<div className="footer__icons">
-						<a
-							className="footer__link"
-							href="https://www.linkedin.com/in/cstatondev/"
-							target="_blank"
-							rel="noopener noreferrer"
-							onClick={() => gaEventTracker("LinkedIn")}
-						>
-							<i class="devicon-linkedin-plain"></i>
-						</a>
-						<a
-							className="footer__link"
-							href="https://github.com/c-staton"
-							target="_blank"
-							rel="noopener noreferrer"
-							onClick={() => gaEventTracker("GitHub")}
-						>
-							<i class="devicon-github-original"></i>
-						</a>
-					</div>
-					<p className="footer__copyright">
-						© 2022 Chris Staton. All rights reserved.
-					</p>
-				</section>
-			</div>
-		</div>
-	);
+    useEffect(() => {
+        ReactGA.pageview(window.location.pathname + window.location.search);
+    }, []);
+
+    return (
+        <div className={`App ${isDark ? "dark-mode" : null}`}>
+            <button
+                className="darkmode"
+                onClick={() => {
+                    switchTheme();
+                    gaEventTracker("Dark Mode");
+                }}
+            >
+                {isDark ? (
+                    <img className="icon" src={Sun} alt="light-mode" />
+                ) : (
+                    <img className="icon" src={Moon} alt="dark-mode" />
+                )}
+            </button>
+            <div className="wrapper">
+                <section className="intro">
+                    <h1
+                        id='my-name'
+                        onClick={() => setClick(1)}
+                        className="intro__h1 shine"
+                        onAnimationEnd={() => setClick(0)}
+                        click={click}
+                    >Chris Staton</h1>
+                    <p className="intro__title">Software Engineer</p>
+                    <p className="intro__paragraph">
+                        I continuously seek emerging technologies to master and incoroparte into my projects.
+                        Every application I undertake is
+                        meticulously crafted from inception to execution,
+                        reflecting a blend of thoughtful planning, design,
+                        and implementation. I'm deeply passionate about
+                        creating not just applications, but distinct digital
+                        brands accentuated with robust SEO and user-centric UI.
+                    </p>
+                </section>
+                <section className="projects">
+                    <Project
+                        title="twomoons.gg"
+                        techTags={["Flask", "Socket.io", "MySQL"]}
+                        image={TMGImg}
+                        link="https://twomoons.gg"
+                        domain="twomoons.gg"
+                    />
+                    <Project
+                        title="evenMoreCode"
+                        techTags={["React", "Node.js", "Prisma"]}
+                        image={EMCImg}
+                        link="https://evenmorecode.com"
+                        domain="evenmorecode.com"
+                    />
+                    <Project
+                        title="Casa Carlos"
+                        techTags={["React", "JavaScript", "CSS"]}
+                        image={CCRImg}
+                        link="https://casacarlosrestaurant.com"
+                        domain="casacarlosrestaurant.com"
+                    />
+                </section>
+                <section className="timeline">
+                    <DevTimeline />
+                </section>
+                <section className="contact">
+                    <ContactForm />
+                </section>
+                <section className="footer">
+                    <div className="footer__icons">
+                        <a
+                            className="footer__link"
+                            href="https://www.linkedin.com/in/cstatondev/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            onClick={() => gaEventTracker("LinkedIn")}
+                        >
+                            <i class="devicon-linkedin-plain"></i>
+                        </a>
+                        <a
+                            className="footer__link"
+                            href="https://github.com/c-staton"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            onClick={() => gaEventTracker("GitHub")}
+                        >
+                            <i class="devicon-github-original"></i>
+                        </a>
+                    </div>
+                    <p className="footer__copyright">
+                        © {CURRENT_YEAR} Chris Staton. All rights reserved.
+                    </p>
+                </section>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/DevTimeline.js
+++ src/DevTimeline.js
@@ -1,46 +1,43 @@
-import React, { useState } from "react";
-import { Timeline, Event } from "react-timeline-scribble";
+import React from "react";
+import { Timeline } from "react-timeline-scribble";
 import TimelineEvent from './TimelineEvent';
 
 const DevTimeline = () => {
-	const [emoteClick1, setEmoteClick1] = useState(0);
-
-
-	return (
-		<div className="dev-timeline">
-			<h1 className="timeline__h1">Timeline</h1>
-			<div className="timeline-comp">
-				<Timeline>
-					<TimelineEvent interval='NOW' emote='👨🏻‍💻' title='I develop software!'>
-						With all my new tools aquired, I found my passion
-						in web development. From concept to completion,
-						I craft each project with a user-centric approach,
-						ensuring an intuitive and seamless experience.
-					</TimelineEvent>
-					<TimelineEvent interval='2022' emote='👷🏻‍♂️' title='I can build things!'>
-						At the beginning of 2022, I took a pivotal step by
-						joining a software engineering bootcamp. This adventure
-						taught me the joys of building full-stack projects
-						from the ground up. The empowerment of turning ideas
-						into tangible applications was nothing short of exhilarating.
-					</TimelineEvent>
-					<TimelineEvent interval='2021' emote='🧐' title='I can do math!'>
-						In college, I tackled courses like the full calculus series,
-						linear algebra, discrete mathematics, and assembly language
-						programming. While these subjects provided me with a rich
-						understanding of coding fundamentals, I recognized I needed
-						to bridge the gap between theoretical knowledge and bringing
-						applications to life.
-					</TimelineEvent>
-					<TimelineEvent interval='2018' emote='🥳' title='I can code!'>
-						I began my coding journey with C++ and MATLAB as my foundational
-						languages. Through the precise and detailed nature of C++, I
-						acquired a robust understanding of core computer science concepts.
-					</TimelineEvent>
-				</Timeline>
-			</div>
-		</div>
-	);
+    return (
+        <div className="dev-timeline">
+            <h1 className="timeline__h1">Timeline</h1>
+            <div className="timeline-comp">
+                <Timeline>
+                    <TimelineEvent interval='NOW' emote='👨🏻‍💻' title='I can software!'>
+                        With all my new tools aquired, I found my passion
+                        in web development. From concept to completion,
+                        I craft each project with a user-centric approach,
+                        ensuring an intuitive and seamless experience.
+                    </TimelineEvent>
+                    <TimelineEvent interval='2022' emote='👷🏻‍♂️' title='I can build!'>
+                        At the beginning of 2022, I took a pivotal step by
+                        joining a software engineering bootcamp. This adventure
+                        taught me the joys of building full-stack projects
+                        from the ground up. The empowerment of turning ideas
+                        into tangible applications was nothing short of exhilarating.
+                    </TimelineEvent>
+                    <TimelineEvent interval='2021' emote='🧐' title='I can math!'>
+                        In college, I tackled courses like the full calculus series,
+                        linear algebra, discrete mathematics, and assembly language
+                        programming. While these subjects provided me with a rich
+                        understanding of computer science fundamentals, I recognized I needed
+                        to bridge the gap between theoretical knowledge and bringing
+                        applications to life.
+                    </TimelineEvent>
+                    <TimelineEvent interval='2018' emote='🥳' title='I can code!'>
+                        I began my coding journey with C++ and MATLAB as my foundational
+                        languages. Through the precise and detailed nature of C++, I
+                        acquired a robust understanding of core programming concepts.
+                    </TimelineEvent>
+                </Timeline>
+            </div>
+        </div>
+    );
 };
 
 export default DevTimeline;
